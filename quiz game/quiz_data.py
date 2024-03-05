82c4a83f8c3fca3484669391021013ff8f95f98f
AmPulaMica <134176298+AmPulaMica@users.noreply.github.com>
2024-03-05 07:12:05 +0200

    Add files via upload

---
+++ quiz game/quiz_data.py
@@ -0,0 +1,63 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "London", "Berlin", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the largest planet in our solar system?",
+        "choices": ["Jupiter", "Saturn", "Mars", "Earth"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is knows as the 'Land of the Rising Sun'?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "In what country did the first Starbucks open outside of North America?",
+        "choices": ["Brazil", "Japan", "North Korea", "London"],
+        "answer": "Japan"
+    },
+    {
+        "question": "Where were the Declaration of Independence, the Constitution, and the Bill of Rights stored during World War II?",
+        "choices": ["China", "Fort Knox", "Romania", "Bangladesh"],
+        "answer": "Fort Knox"
+    },
+    {
+        "question": "Which company's slogan is: (You are in good hands)?",
+        "choices": ["Ikea", "McDonalds", "KFC", "AllState"],
+        "answer": "AllState"
+    },
+    {
+        "question": "Who was the first televised President?",
+        "choices": ["Franklin D. Roosevelt", "George Washington", "John Adams", "James Madison"],
+        "answer": "Franklin D. Roosevelt"
+    },
+    {
+        "question": "Originally, Amazon only sold what kind of product?",
+        "choices": ["Drugs", "Cars", "Books", "Computers"],
+        "answer": "Books"
+    },
+    {
+        "question": "In 2009, what became the first Morse code character to be added since WWII?",
+        "choices": ["The @ symbol", "The # symbol", "The ^ symbol", "The ! symbol"],
+        "answer": "The @ symbol"
+    },
+    {
+        "question": "Who painted the Mona Lisa?",
+        "choices": ["Leonardo da Vinci", "Vincent Van G", "Pablo Picasso", "Frans Hals"],
+        "answer": "Leonardo da Vinci"
+    },
+    {
+        "question": "Who was the first woman to win a Nobel Prize?",
+        "choices": ["Ada Yonath", "Gerty Cori", "Marie Curie", "Alva Myrdal"],
+        "answer": "Marie Curie"
+    }
+    # Add more questions here
+]
